fe7ab266c5a3d83982cf46157216ffa907ffe645
roger guo <rogerguo3791@sina.com>
2020-12-15 23:32:54 +0800

    add property test codes

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <map>
 #include <string> 
+#include <assert.h>
 #include <initializer_list>
 #include <pigpiod_if2.h>
 #include <iostream>
@@ -135,12 +136,100 @@ void dict_test() {
 }
 map<int,string> reservations{{1,"dfd"},{2,"dfgg"}};
 
+#define READ_ONLY 1
+#define WRITE_ONLY 2
+#define READ_WRITE 3
+template <typename Container, typename ValueType, int nPropType>
+class property
+{
+        public:
+                property(){  
+                 m_cObject = NULL;       
+                       Set = NULL;
+                       Get = NULL;
+                }
+ 
+        //-- This to set a pointer to the class that contain the
+        //   property --
+ 
+        void setContainer(Container* cObject){
+                m_cObject = cObject;
+        }
+ 
+        //-- Set the set member function that will change the value --
+        void setter(void (Container::*pSet)(ValueType value)){
+                if((nPropType == WRITE_ONLY) || (nPropType == READ_WRITE))
+                        Set = pSet;
+                else
+                        Set = NULL;
+        }
+ 
+                //-- Set the get member function that will retrieve the value --
+        void getter(ValueType (Container::*pGet)()){
+                if((nPropType == READ_ONLY) || (nPropType == READ_WRITE))
+                        Get = pGet;
+                else
+                        Get = NULL;
+        }
+ 
+        //-- Overload the '=' sign to set the value using the set
+        //   member --
+ 
+        ValueType operator =(const ValueType& value){
+                assert(m_cObject != NULL);
+                assert(Set != NULL);
+                (m_cObject->*Set)(value);
+                return value;
+        }
+ 
+        //-- To make possible to cast the property class to the
+        //   internal type --
+ 
+        operator ValueType(){
+                assert(m_cObject != NULL);
+                assert(Get != NULL);
+                return (m_cObject->*Get)();
+        }
+        private:
+        Container* m_cObject;  //-- Pointer to the module that
+                                //   contains the property --
+        void (Container::*Set)(ValueType value);
+                                //-- Pointer to set member function --
+        ValueType (Container::*Get)();
+                                //-- Pointer to get member function --
+ };
+
+ class PropTest{
+         public:
+ 
+        PropTest(){
+                Count.setContainer(this);
+                Count.setter(&PropTest::setCount); 
+                Count.getter(&PropTest::getCount);
+        }
+   int getCount()  {
+     return m_nCount;
+   }
+ 
+  void setCount(int nCount){
+     m_nCount = nCount;
+   }
+   property<PropTest,int,READ_WRITE> Count;
+        private:
+             int m_nCount;
+};
 
 int main(int argc, char* argv[]){
-        pi_info();
+       // pi_info();
        //list_test();
        // dict_test();
-
-  
+        int i = 5,j;
+        
+        PropTest test;
+        
+        test.Count = i;    //-- call the set method --
+        
+        j= test.Count;     //-- call the get method --
+        cout<<"i:"<<i<<"  "<<"j:"<<j<<endl;
     return 0;
 }
